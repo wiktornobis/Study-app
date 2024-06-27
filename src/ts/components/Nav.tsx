1991efbccce49deb1d2d8ceac82c99b1e2ead319
wiktor <nobis171@wp.pl>
2024-06-27 18:05:55 +0200

    init responsive nav

--- src/ts/components/Nav.tsx
+++ src/ts/components/Nav.tsx
@@ -1,28 +1,147 @@
-import React, { useContext } from 'react';
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
-import { Brightness4, Brightness7 } from '@mui/icons-material';
-import { ColorModeContext } from '@/ts/theme/ToggleColorMode';
+import * as React from 'react';
+import {useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {Brightness4, Brightness7} from '@mui/icons-material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
-const Nav: React.FC = () => {
+import {ColorModeContext} from '@/ts/theme/ToggleColorMode';
+
+const pages = [
+    { id: 1, name: 'Newsy', path: '/newsy' },
+    { id: 2, name: 'Dane', path: '/dane' },
+    { id: 3, name: 'Uczelnie', path: '/uczelnie' }
+];
+
+function Nav() {
     const colorMode = useContext(ColorModeContext);
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const navigate = useNavigate();
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>):void => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = ():void => {
+        setAnchorElNav(null);
+    };
+
+    const handleNavigation = (path: string):void => {
+        navigate(path);
+        handleCloseNavMenu();
+    };
 
     return (
         <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Nav
-                </Typography>
-                <IconButton
-                    edge="end"
-                    color="inherit"
-                    aria-label="mode"
-                    onClick={colorMode.toggleColorMode}
-                >
-                    {colorMode.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
-                </IconButton>
-            </Toolbar>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page.id} onClick={() => handleNavigation(page.path)}>
+                                    <Typography textAlign="center">{page.name}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.id}
+                                onClick={() => handleNavigation(page.path)}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+                    <IconButton
+                        edge="end"
+                        color="inherit"
+                        aria-label="mode"
+                        onClick={colorMode.toggleColorMode}
+                    >
+                        {colorMode.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+                    </IconButton>
+
+                </Toolbar>
+            </Container>
         </AppBar>
     );
-};
+}
 
 export default Nav;
